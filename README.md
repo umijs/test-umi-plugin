15c0c8f220e8c5b89847f76c290564c60c8ff720
chencheng <sorrycc@gmail.com>
2019-04-26 15:21:27 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# test-umi-plugin
+
+Test util for umi plugin.
+
+## Install
+
+Install as dev dependency.
+
+```bash
+$ yarn add test-umi-plugin -D
+```
+
+## Usage
+
+```js
+const testUmiPlugin = require('test-umi-plugin');
+testUmiPlugin(options);
+```
+
+## Options
+
+### fixtures
+
+* Type: String
+
+### basePort
+
+* Type: Number
+* Default: 12400
+
+## LICENSE
+
+MIT
---
+++ index.js
@@ -0,0 +1,89 @@
+const { fork } = require('child_process');
+const { join } = require('path');
+const http = require('http');
+const puppeteer = require('puppeteer');
+const { existsSync, readdirSync } = require('fs');
+
+module.exports = function(opts = {}) {
+  const {
+    fixtures,
+    basePort,
+  } = opts;
+
+  let port = basePort || 12400;
+  let browser;
+  let page;
+  const servers = {};
+
+  let dirs = readdirSync(fixtures).filter(dir => dir.charAt(0) !== '.');
+  if (dirs.some(dir => dir.includes('-only'))) {
+    dirs = dirs.filter(dir => dir.includes('-only'));
+  }
+
+  async function serve(base, key) {
+    return new Promise((resolve) => {
+      port += 1;
+      servers[key] = { port };
+      servers[key].server = http.createServer((request, response) => {
+        return require('serve-static')(base)(request, response);
+      });
+      servers[key].server.listen(port, () => {
+        console.log(`[${key}] Running at http://localhost:${port}`);
+        resolve();
+      });
+    });
+  }
+
+  async function build(base) {
+    return new Promise((resolve, reject) => {
+      const umiPath = join(process.cwd(), './node_modules/umi/bin/umi.js');
+      const env = {
+        COMPRESS: 'none',
+        PROGRESS: 'none',
+      };
+      const child = fork(umiPath, ['build'], {
+        cwd: base,
+        env,
+      });
+      child.on('exit', code => {
+        if (code === 1) {
+          reject(new Error('Build failed'));
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  beforeAll(async () => {
+    for (const dir of dirs) {
+      const base = join(fixtures, dir);
+      const targetDist = join(base, 'dist');
+      if (!existsSync(targetDist)) {
+        await build(base);
+      }
+      await serve(targetDist, dir);
+    }
+    browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+  });
+
+  beforeEach(async () => {
+    page = await browser.newPage();
+  });
+
+  for (const dir of dirs) {
+    test(dir, async () => {
+      await require(join(fixtures, `${dir}/test`)).default({
+        page,
+        host: `http://localhost:${servers[dir].port}`,
+      });
+    });
+  }
+
+  afterAll(() => {
+    Object.keys(servers).forEach(key => {
+      servers[key].server.close();
+    });
+    if (browser) browser.close();
+  });
+}
